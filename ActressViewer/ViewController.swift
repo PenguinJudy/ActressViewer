e374735087a5142461b8246de5134a7b197637fe
yudai <draco.anguis.17yt@gmail.com>
2019-06-21 11:46:59 +0900

    Initial Commit

---
+++ ActressViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ActressViewer
+//
+//  Created by 辰巳優大 on 2019/06/21.
+//  Copyright © 2019 Yudai Tatsumi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
